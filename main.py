c260fd692c9b253dfcd83f786ea1c45cec1e0e24
USER <ruppa.santosh143darling@gmail.com>
2023-05-15 14:29:13 +0530

    usinf append method to remove duplicate values from the list

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-sqare = [i**2 for i in range(4, 40)]
-print(sqare)
-########################
+# removing duplicate values from the list
+lst = [10, 20, 10, 30, 20, 40, 50, 30]
+new_lst = []
+
+for i in lst:
+    if i not in new_lst:
+        new_lst.append(i)
+
+print("List with duplicates removed:", new_lst)
+
